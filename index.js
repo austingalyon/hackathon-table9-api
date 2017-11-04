a312add27405e3beba7c04c45f0e007696ea5c59
Patrick Gilday <pcgilday@users.noreply.github.com>
2017-11-04 16:38:16 -0400

    Add basic accounts fetch

--- index.js
+++ index.js
@@ -5,18 +5,32 @@ const app = express()
 
 const accountsEndpoint = 'https://3hkaob4gkc.execute-api.us-east-1.amazonaws.com/prod/au-hackathon/accounts'
 
+const headers = {
+    "access-control-allow-origin": "*",
+    "access-control-allow-credentials": true,
+    "access-control-allow-methods": "*"
+}
+
+
 function getAccounts() {
     return agent
         .post(accountsEndpoint)
         .send({})
 }
 
+app.use(function(req, res, next) {
+  res.header("access-control-allow-origin", "*");
+    console.log('mayyybbee')
+  next();
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/accounts', (req, res) =>
     getAccounts()
         .then((response) => {
             console.log('response', response)
+            res.header['access-control-allow-origin'] = '*'
+            res.header['access-control-allow-methods'] =  "*"
             return res.send(response)
         }))
 
